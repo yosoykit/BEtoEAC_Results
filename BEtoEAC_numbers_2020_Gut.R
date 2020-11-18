4373e6a2bb788633cb2543c0a8eaf608b930b4b3
yosoykit <kitcurtius@gmail.com>
2020-11-18 09:26:13 -0800

    cleaned up section headings

--- BEtoEAC_numbers_2020_Gut.R
+++ BEtoEAC_numbers_2020_Gut.R
@@ -7,11 +7,15 @@ source('/BaseCaseII/legauss.R')
 dyn.load('/BaseCaseII/legauss.so')
 library('deSolve')
 
+###################################################################################
+### Functions for 1) solving Kolmogorov backward equations from MSCE-EAC model  ###
+##     using system of ODES, 2) model hazard function, 3) BE density function   ###
+###################################################################################
+
 phidot = function(s, phi, parms){
   phidot=numeric(6)
   with(as.list(parms),{
   tau = t - s
-  #RR=4
   GERDpr=1-exp(-gerd1*min(gerd3,tau)-gerd2*max(0,tau-gerd3))#/1.5
   nu = nu0*((1-GERDpr)+RR*GERDpr)
   phidot[1]=betam -(alpham+betam+rho)*phi[1]+alpham*(phi[1]**2)
@@ -33,9 +37,7 @@ phidot_GERD = function(s, phi, parms){
   phidot=numeric(6)
   with(as.list(parms),{
   tau = t - s
-  #RR=4
   GERDpr=1-exp(-gerd1*min(gerd3,tau)-gerd2*max(0,tau-gerd3))
-  #nu = nu0*((1-GERDpr)+RR*GERDpr)
   nu = nu0*(RR*1)
   phidot[1]=betam -(alpham+betam+rho)*phi[1]+alpham*(phi[1]**2)
   phidot[2]=2*alpham*phi[1]*phi[2]- (alpham+betam+rho)*phi[2]
@@ -159,8 +161,9 @@ BEdensity_GERD <- function(screen_age,gerd1,gerd2,gerd3,nu0_temp,RR_temp){
   return(list(nu=nu,nu_cum=nu_cum,nu_dens=nu_dens,GERDpr=GERDpr1))
 }
 
-
-## US CENSUS DATA FOR 2010 ####
+######################################################
+## US CENSUS DATA FOR 2010 for at-risk population ####
+######################################################
 
 m_num_2010 = c(10393977,11209085,10933274,9523648,8077500,5852547,4243972,3182388,2294374,1273867,424387,82263,9162)
 f_num_2010 = c(10496987,11499506,11364851,10141157,8740424,6582716,5034194,4135407,3448953,2346592,1023979,288981,44202)
@@ -169,7 +172,7 @@ print(totalpop_2010)
 ## 142,648,393
 
 
-## NUMBER OF BE CASES
+## NUMBER OF BE CASES estimated by model (secondary outcome)
 b_c1 = 1950
 birth_cohort = b_c1
 RR=5
@@ -177,7 +180,7 @@ age_groups = seq(42.5,102.5,by=5)
 source('am_parameter_list_2020.R')
 
 nu_cum_m = BEdensity(age_groups[length(age_groups)],gerd1,gerd2,gerd3,nu0,RR)$nu_cum
-source('/Users/curtiu01/Dropbox/BaseCaseII/af_parameter_list.R')
+source('af_parameter_list_2020.R')
 
 nu_cum_f = BEdensity(age_groups[length(age_groups)],gerd1,gerd2,gerd3,nu0,RR)$nu_cum
 ## Choose BE prevalence (unconditional on EAC) for 5 year age groups 40+ to 100+
@@ -198,7 +201,7 @@ print(sum(m_num_2010,f_num_2010))
 #  142,648,393
 
 #########################################################################################################
-###############################     FOR NUMBERS PAPER RESULTS         ###################################
+###############################     FOR EAC NUMBERS PAPER RESULTS         ###############################
 ################### SEER Explorer population incidence rates for expected cases in 2010    ##############
 #########################################################################################################
 
@@ -307,6 +310,9 @@ get_params = function(gender,bc){
   }
 }
 
+##############################################################################
+###         RESULTS FOR predicted EAC cases as primary outcome             ###
+##############################################################################
 
 age_groups = seq(42.5,87.5,by=5)
 Lambda_m = rep(0,length(age_groups))
@@ -331,9 +337,10 @@ print(totalcases_2010)
 
 
 
-#########################################################################################################
-### MAIN RESULTS FOR corresponding BE prevalence-  VALIDATION WITH CORI FOR MALES/FEMALES             ###
-#########################################################################################################
+###############################################################################
+### RESULTS FOR corresponding predicted BE prevalence as secondary outcome  ###
+##                  -  VALIDATION WITH CORI FOR MALES/FEMALES               ###
+###############################################################################
 
 #####################################################################################
 #################   FIGURE 3 for differing RR values            #####################
@@ -605,21 +612,16 @@ lines(t_s[seq(16,66,1)],strat1_w_noMortality50[seq(16,66,1),5]*100,type='l',lwd=
 polygon(c(t_s[seq(16,66,1)],t_s[seq(66,16)]),c(strat1_w_f_noMortality50_2[seq(16,66),5]*100,strat1_w_f_noMortality50_6[seq(66,16),5]*100),col= adjustcolor( "orange", alpha.f = 0.1),border=adjustcolor( "orange", alpha.f = 0.1))
 polygon(c(t_s[seq(16,66,1)],t_s[seq(66,16)]),c(strat1_w_noMortality50_2[seq(16,66),5]*100,strat1_w_noMortality50_6[seq(66,16),5]*100),col= adjustcolor( "green3", alpha.f = 0.1),border=adjustcolor( "green3", alpha.f = 0.1))
 
-legend('topleft',c('Model: Males', 'CORI Males: general', 'Model: Females','CORI Females: general' ), col=c("green3", "green3", "orange","orange"),cex=1.5, lwd = 3, bty='n',lty=c(1,3,1,3))
-
-
 plot(t_s[seq(16,66,1)],strat1_w_GERD_noMortality50[seq(16,66,1),5]*100,type='l',lwd=3,col="blue",lty=1,pch=15,cex.axis=2, cex.lab=2, cex=2, ylim=c(0,13),xlim=c(20,80),ylab= "BE Prevalence in non-EAC (%)", xlab="Age")
 lines(t_s[seq(16,66,1)],strat1_w_GERD_f_noMortality50[seq(16,66,1),5]*100,type='l',lwd=3,col="red",lty=1,pch=16,cex=1)
-
-
 polygon(c(t_s[seq(16,66,1)],t_s[seq(66,16)]),c(strat1_w_GERD_noMortality50_2[seq(16,66),5]*100,strat1_w_GERD_noMortality50_6[seq(66,16),5]*100),col= adjustcolor( "blue", alpha.f = 0.1),border=adjustcolor( "blue", alpha.f = 0.1))
 polygon(c(t_s[seq(16,66,1)],t_s[seq(66,16)]),c(strat1_w_GERD_f_noMortality50_2[seq(16,66),5]*100,strat1_w_GERD_f_noMortality50_6[seq(66,16),5]*100),col= adjustcolor( "red", alpha.f = 0.1),border=adjustcolor( "red", alpha.f = 0.1))
 
-legend('topleft',c('Model: GERD Males', 'CORI Males: screening', 'Model: GERD Females','CORI Females: screening' ), col=c("blue", "blue", "red","red"),cex=1, lwd = 3, bty='n',lty=c(1,3,1,3))
 
 
-
-##  CHANGING GERD PREV, include in Supplementary material , Figure S1
+#############################################################################################
+##  Sensitivity Analysis: CHANGING GERD PREV, include in Supplementary material , Figure S1 #
+#############################################################################################
 w=seq(0,20, .25)
 RR=5
 birth_cohort = 1950
@@ -843,9 +845,10 @@ for (i in 1:length(w)){
 strat1_w_noMortality50_low=strat1_w_noMortality
 strat1_w_f_noMortality50_low=strat1_w_f_noMortality
 
-## FOR GERD PREV, Figure S1
+## Figure S1
 dev.new()
 par(mfrow=c(2,2),xaxs="i", yaxs="i", las = 1, mar=c(5,5,2,3), bty='l')
+
 plot(t_s[seq(11,66,1)],GERD_cum[seq(11,66,1)]*100,type='l',lwd=3,col="blue",lty=1,pch=17,cex=1.5,cex.axis=1.5, cex.lab=1.5, ylim=c(0,50),xlim=c(20,80),ylab= "GERD prevalence(%)", xlab="Age")
 lines(t_s[seq(11,66,1)],GERD_cum_high[seq(11,66,1)]*100,type='l',lwd=3,col="goldenrod",lty=1,pch=17,cex=1)
 lines(t_s[seq(11,66,1)],GERD_cum_low[seq(11,66,1)]*100,type='l',lwd=3,col="goldenrod",lty=4,pch=17,cex=1)
@@ -858,23 +861,43 @@ lines(t_s[seq(11,66,1)],GERD_cumf_low[seq(11,66,1)]*100,type='l',lwd=3,col="purp
 polygon(c(t_s[seq(11,66,1)],t_s[seq(66,11)]),c(GERD_cumf_low[seq(11,66)]*100,GERD_cumf_high[seq(66,11)]*100),col= adjustcolor( "purple", alpha.f = 0.1),border=adjustcolor( "purple", alpha.f = 0.1))
 legend('topleft',c('GERD Females: 50% increase', 'GERD Femles: baseline prev', 'GERD Females: 50% decrease' ), col=c("purple", "red","purple"),cex=1, lwd = 3, bty='n',lty=c(4,1,1))
 
-
 plot(t_s[seq(16,66,1)],strat1_w_noMortality50_low[seq(16,66,1),5]*100,type='l',lwd=3,col="goldenrod",lty=1,pch=17,cex=1.5,cex.axis=1.5, cex.lab=1.5, ylim=c(0,6),xlim=c(20,80),ylab= "BE Prevalence in non-EAC (%)", xlab="Age")
 lines(t_s[seq(16,66,1)],strat1_w_noMortality50_high[seq(16,66,1),5]*100,type='l',lwd=3,col="goldenrod",lty=4,pch=17,cex=1.5,cex.axis=1.5, cex.lab=1.5, ylim=c(0,6),xlim=c(20,80),ylab= "BE Prevalence in non-EAC (%)", xlab="Age")
-
 polygon(c(t_s[seq(16,66,1)],t_s[seq(66,16)]),c(strat1_w_noMortality50_low[seq(16,66),5]*100,strat1_w_noMortality50_high[seq(66,16),5]*100),col= adjustcolor( "goldenrod", alpha.f = 0.1),border=adjustcolor( "goldenrod", alpha.f = 0.1))
-
 
 plot(t_s[seq(16,66,1)],strat1_w_f_noMortality50_low[seq(16,66,1),5]*100,type='l',lwd=3,col="purple",lty=1,pch=17,cex=1.5,cex.axis=1.5, cex.lab=1.5, ylim=c(0,6),xlim=c(20,80),ylab= "BE Prevalence in non-EAC (%)", xlab="Age")
 lines(t_s[seq(16,66,1)],strat1_w_f_noMortality50_high[seq(16,66,1),5]*100,type='l',lwd=3,col="purple",lty=4,pch=17,cex=1.5,cex.axis=1.5, cex.lab=1.5, ylim=c(0,6),xlim=c(20,80),ylab= "BE Prevalence in non-EAC (%)", xlab="Age")
-
 polygon(c(t_s[seq(16,66,1)],t_s[seq(66,16)]),c(strat1_w_f_noMortality50_low[seq(16,66),5]*100,strat1_w_f_noMortality50_high[seq(66,16),5]*100),col= adjustcolor( "purple", alpha.f = 0.1),border=adjustcolor( "purple", alpha.f = 0.1))
 
 
 
+##################################################################################################
+## Sensitivity Analysis:: How many EACs does the model predict if BE prev in GERD               ##
+##                                subpopulation was lower: Figure S2.                           ##
+##                                     Let's first try for RR=3                                 ##
+##################################################################################################
 
-## How many EACs does the model predict if BE prev in GERD was lower: Figure S2
-## Let's first try for RR=3
+phidot = function(s, phi, parms){
+  phidot=numeric(6)
+  with(as.list(parms),{
+  tau = t - s
+  GERDpr=1-exp(-gerd1*min(gerd3,tau)-gerd2*max(0,tau-gerd3))#/1.5
+  nu = nu0*((1-GERDpr)+RR*GERDpr)
+  phidot[1]=betam -(alpham+betam+rho)*phi[1]+alpham*(phi[1]**2)
+  phidot[2]=2*alpham*phi[1]*phi[2]- (alpham+betam+rho)*phi[2]
+  phidot[3]=betap+ mu2*phi[1]*phi[3]-(alphap+betap+mu2)*phi[3]+alphap*(phi[3]**2)
+  phidot[4]=2*alphap*phi[3]*phi[4]+mu2*(phi[4]*phi[1]+phi[3]*phi[2]) - (alphap+betap+mu2)*phi[4]
+  phidot[5]=mu1*phi[5]*(phi[3]-1)
+  phidot[6]=mu1*(phi[6]*(phi[3]-1)+phi[5]*phi[4])
+  phidot[7]=mu0*phi[7]*(phi[5]-1)
+  phidot[8]=mu0*(phi[8]*(phi[5]-1)+phi[7]*phi[6])
+  ## Armitage-Doll transition
+  phidot[9]=nu*(phi[7]-phi[9])
+  phidot[10]=nu*(phi[8]-phi[10])
+  list(c(phidot))
+  })
+}
+
 
 w=seq(0,20, .25)
 RR=3
@@ -1045,7 +1068,7 @@ strat1_w_f_noMortality50_4=strat1_w_f_noMortality
 strat1_w_GERD_f_noMortality50_4 = strat1_w_GERD_f_noMortality
 
 
-## NEW FIGURE 3 A-B for second revision, Figure S2
+## Figure S2
 dev.new()
 par(mfrow=c(1,2),xaxs="i", yaxs="i", las = 1, mar=c(5,5,2,3), bty='l')
 
@@ -1056,11 +1079,8 @@ lines(t_s[seq(16,66,1)],strat1_w_f_noMortality50_3[seq(16,66,1),5]*100,type='l',
 lines(t_s[seq(16,66,1)],strat1_w_noMortality50_4[seq(16,66,1),5]*100,type='l',lwd=3,col="darkgrey",lty=2,pch=15,cex.axis=2, cex.lab=2, cex=2, ylim=c(0,13),xlim=c(20,80),ylab= "BE Prevalence in non-EAC (%)", xlab="Age")
 lines(t_s[seq(16,66,1)],strat1_w_f_noMortality50_4[seq(16,66,1),5]*100,type='l',lwd=3,col="darkgrey",lty=2,pch=16,cex=1)
 
-
 polygon(c(t_s[seq(16,66,1)],t_s[seq(66,16)]),c(strat1_w_f_noMortality50_2[seq(16,66),5]*100,strat1_w_f_noMortality50_6[seq(66,16),5]*100),col= adjustcolor( "orange", alpha.f = 0.1),border=adjustcolor( "orange", alpha.f = 0.1))
 polygon(c(t_s[seq(16,66,1)],t_s[seq(66,16)]),c(strat1_w_noMortality50_2[seq(16,66),5]*100,strat1_w_noMortality50_6[seq(66,16),5]*100),col= adjustcolor( "green3", alpha.f = 0.1),border=adjustcolor( "green3", alpha.f = 0.1))
-
-
 
 plot(t_s[seq(16,66,1)],strat1_w_GERD_noMortality50[seq(16,66,1),5]*100,type='l',lwd=3,col="blue",lty=1,pch=15,cex.axis=2, cex.lab=2, cex=2, ylim=c(0,13),xlim=c(20,80),ylab= "BE Prevalence in non-EAC (%)", xlab="Age")
 lines(t_s[seq(16,66,1)],strat1_w_GERD_f_noMortality50[seq(16,66,1),5]*100,type='l',lwd=3,col="red",lty=1,pch=16,cex=1)
@@ -1069,36 +1089,12 @@ lines(t_s[seq(16,66,1)],strat1_w_GERD_f_noMortality50_3[seq(16,66,1),5]*100,type
 lines(t_s[seq(16,66,1)],strat1_w_GERD_noMortality50_4[seq(16,66,1),5]*100,type='l',lwd=3,col="darkgrey",lty=2,pch=15,cex.axis=2, cex.lab=2, cex=2, ylim=c(0,13),xlim=c(20,80),ylab= "BE Prevalence in non-EAC (%)", xlab="Age")
 lines(t_s[seq(16,66,1)],strat1_w_GERD_f_noMortality50_4[seq(16,66,1),5]*100,type='l',lwd=3,col="darkgrey",lty=2,pch=16,cex=1)
 
-
 polygon(c(t_s[seq(16,66,1)],t_s[seq(66,16)]),c(strat1_w_GERD_noMortality50_2[seq(16,66),5]*100,strat1_w_GERD_noMortality50_6[seq(66,16),5]*100),col= adjustcolor( "blue", alpha.f = 0.1),border=adjustcolor( "blue", alpha.f = 0.1))
 polygon(c(t_s[seq(16,66,1)],t_s[seq(66,16)]),c(strat1_w_GERD_f_noMortality50_2[seq(16,66),5]*100,strat1_w_GERD_f_noMortality50_6[seq(66,16),5]*100),col= adjustcolor( "red", alpha.f = 0.1),border=adjustcolor( "red", alpha.f = 0.1))
 
-
-
-
-## Compute number of EACs from much lower RR =3,4
-phidot = function(s, phi, parms){
-  phidot=numeric(6)
-  with(as.list(parms),{
-  tau = t - s
-  #RR=4
-  GERDpr=1-exp(-gerd1*min(gerd3,tau)-gerd2*max(0,tau-gerd3))#/1.5
-  nu = nu0*((1-GERDpr)+RR*GERDpr)
-  phidot[1]=betam -(alpham+betam+rho)*phi[1]+alpham*(phi[1]**2)
-  phidot[2]=2*alpham*phi[1]*phi[2]- (alpham+betam+rho)*phi[2]
-  phidot[3]=betap+ mu2*phi[1]*phi[3]-(alphap+betap+mu2)*phi[3]+alphap*(phi[3]**2)
-  phidot[4]=2*alphap*phi[3]*phi[4]+mu2*(phi[4]*phi[1]+phi[3]*phi[2]) - (alphap+betap+mu2)*phi[4]
-  phidot[5]=mu1*phi[5]*(phi[3]-1)
-  phidot[6]=mu1*(phi[6]*(phi[3]-1)+phi[5]*phi[4])
-  phidot[7]=mu0*phi[7]*(phi[5]-1)
-  phidot[8]=mu0*(phi[8]*(phi[5]-1)+phi[7]*phi[6])
-  ## Armitage-Doll transition
-  phidot[9]=nu*(phi[7]-phi[9])
-  phidot[10]=nu*(phi[8]-phi[10])
-  list(c(phidot))
-  })
-}
-
+############################################################################## 
+## Compute number of EACs that arise when assuming much lower RR =3,4      ###
+##############################################################################
 RR=3
 age_groups = seq(42.5,87.5,by=5)
 Lambda_m = rep(0,length(age_groups))
@@ -1140,13 +1136,17 @@ totalcases_2010=sum(Lambda_f,Lambda_m)
 print(totalcases_2010)
 ## [1] 9904.47
 
+## Calculates BE prevalence % at age 55 and 75 for RR=3 and 4, in Supplementary
 ind55= which(t_s[seq(16,66,1)]==55)
 ind75 = which(t_s[seq(16,66,1)]==75)
 
 print(strat1_w_GERD_noMortality50_3[seq(16,66,1),5][ind55]*100)
+#[1] 5.715635
 print(strat1_w_GERD_noMortality50_4[seq(16,66,1),5][ind55]*100)
+#[1] 7.547573
 
 print(strat1_w_GERD_noMortality50_3[seq(16,66,1),5][ind75]*100)
+#[1] 6.568315
 print(strat1_w_GERD_noMortality50_4[seq(16,66,1),5][ind75]*100)
-
+#[1] 8.662434
 
